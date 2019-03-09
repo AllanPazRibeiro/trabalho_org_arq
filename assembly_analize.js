aab83cc511a6d9dbc02e5663099ca08d08911680
allan ribeiro <allan_paz_ribeiro@hotmail.com>
2019-03-09 19:04:00 -0300

    aaaaaaaa

--- assembly_analize.js
+++ assembly_analize.js
@@ -6,35 +6,36 @@ const leArquivo = () => {
 	let lines = data.split('\n');
 	var variables_next_arr = [];
 	var variables_arr = [];
+	var inst_arr = [];
+	var inst_next_arr = [];
 
-	for(let line = 0; line < lines.length; line++){
+	for (line in lines) {
 		//let words = lines[line].split('\t');
 		let variables = lines[line].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g);
-		if (variables != null && variables.length > 0){
-			variables_arr.push(variables);
-		}
+		let inst = lines[line].match(/ADDI|ADD|SUB|LW|J|BNE/g);
+		inst_arr.push(inst);
+		variables_arr.push(variables);
+	}
+	for (let line2 = 1; line2 < lines.length; line2++){
+		let variables_next = lines[line2].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g);
+		let inst2 = lines[line2].match(/ADDI|ADD|SUB|LW|J|BNE/g);
+		inst_next_arr.push(inst2);
+		variables_next_arr.push(variables_next);
 	}
 
-	//Pula 1
-	for(let line2 = 1; line2 < lines.length; line2++){
-		let variables = lines[line2].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g);
-		if (variables != null && variables.length > 0) {
-			variables_next_arr.push(variables);
-		}
-	}
-	
-		console.log('\n');
-		console.log('-------------variables_arr--------------------------------------------');
-		console.log(variables_arr+ '\n');
-		console.log('----------------------------------------------------------------------');
-		console.log('\n');
-		console.log('-------------variables_next_arr---------------------------------------');
-		console.log(variables_next_arr + '\n');
-		console.log('\n');
-		console.log('----------------------------------------------------------------------');
-		console.log('\n');
+	// //Pula 1
+	// for (let line2 = 1; line2 < lines.length; line2++) {
+	// 	let variables = lines[line2 + 1].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g);
+	// 	let inst = lines[line2 + 1].match(/ADDI|ADD|SUB|LW|J|BNE/g);
+	// 	if (variables != null && variables.length > 0)
+	// 		variables_next_arr.push(variables);
+	// 	if(inst != undefined && inst.length > 0)
+	// 		inst_next_arr.push(inst);
+		
+	// }
+		
 		console.log('-------------intersection---------------------------------------------');
-		console.log(intersection(variables_arr, variables_next_arr));
+		intersection(variables_arr, variables_next_arr, inst_arr, inst_next_arr);
 		console.log('----------------------------------------------------------------------');
 		console.log('\n');
 		
@@ -43,16 +44,24 @@ const leArquivo = () => {
 leArquivo();
 
 //Verifica a intersecção
-const intersection = (a, b) => {
+var intersection = (a, b, inst, inst_next) => {
 	var aux = [];
 	var aux2 = [];
-	for(let i = 0; i < a.length; i++) {
-		aux.push(a[i]);
-	}
+	var aux3 = [];
+	var aux4 = [];
+	var aux5 = [];
+	console.log(inst_next);
 
-	for(let j = 0; j < b.length; j++) {
-		aux2.push(b[j]);
+	a.map( value => aux.push(value));
+	b.map( value => aux2.push(value));
+	inst.map( value => aux3.push(value));
+	inst_next.map( value => aux4.push(value));
+
+	// console.log((aux.filter((value) => aux2.includes(value)+"\n")));
+	for (i in (aux.filter((value) => aux2.includes(value)+"\n"))){
+		//if (aux.filter((value) => aux2.includes(value)+"\n"))
+		console.log(inst[i] + " tem conflito com " + inst_next[i]);
 	}
-	return aux.filter(value => aux2.includes(value)+"\n");
+	
 }
 
