b7ad883f34f24af56f95180bd178edb6a3ed0da4
allan ribeiro <allan_paz_ribeiro@hotmail.com>
2019-03-17 23:17:33 -0300

    aaaaaaaaaaaaaa

--- assembly_analize.js
+++ assembly_analize.js
@@ -57,78 +57,44 @@
 // 	}
 	
 // }
-
-
+const _ = require('lodash');
 const fs = require('fs');
 
 const ParseArquivo = () => {
 	fs.readFile('assembly.txt', 'utf8', function(err, data) { 
 	let lines = data.split('\n');
-	var variables_next_arr = [];
-	var variables_arr = [];
-	var inst_arr = [];
-	var inst_next_arr = [];
-	var par_arr = [];
-	var par_next_arr = [];
-
-	for (line in lines) {
-		//let words = lines[line].split('\t');
-		let variables = lines[line].match(/((\$[TS])\S?\d,\$[TS]\d,)\S((\$[TS]\d)|(\w*)\S)?/g);
-		let inst = lines[line].match(/ADDI|ADD|SUB|LW|J|BNE/g);
-		let variables_next = lines[parseInt(line) + 1] ? lines[parseInt(line)  + 1].match(/((\$[TS])\S?\d,\$[TS]\d,)\S((\$[TS]\d)|(\w*)\S)?/g) : '';
-		let inst2 = lines[parseInt(line)  + 1] ? lines[parseInt(line) + 1].match(/ADDI|ADD|SUB|LW|J|BNE/g) : '';
-		let par = lines[line].match(/\$([TS])\S/g);
-		let par_next = lines[parseInt(line)  + 1] ? lines[parseInt(line) + 1].match(/\$([TS])\S/g) : '';
-		
-		inst_arr.push(inst);
-		variables_arr.push(variables);
-		par_arr.push(par);
-		
-		inst_next_arr.push(inst2);
-		variables_next_arr.push(variables_next);
-		par_next_arr.push(par_next);
-
-	}		
+	let aux = lines.toString().split('\t\tADD\t$T0,$T1,$T2\r');
+	let lines_next = aux.toString().split('\r');
+	var linesObj = [];
+	var linesObjNext = [];
+	lines.map(data => {
+		linesObj.push({
+			linesItSelf : data,
+			inst: data.match(/ADDI|ADD|SUB|LW|J|BNE/g),
+			parameters: data.match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g)
+		})
+	});
+	lines_next.map(data => {
+		linesObjNext.push({
+			linesItSelf : data,
+			inst: data.match(/ADDI|ADD|SUB|LW|J|BNE/g),
+			parameters: data.match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g)
+		})
+	});	
+		console.log();
 		console.log('-------------intersection---------------------------------------------');
-		intersection(variables_arr, inst_arr, par_arr, variables_next_arr, inst_next_arr, par_next_arr);
+		console.log(JSON.stringify(linesObj), JSON.stringify(linesObjNext));
 		console.log('----------------------------------------------------------------------');
 		console.log('\n');
-		
 	});
 }
 ParseArquivo();
 
-//Verifica a intersecção
-var intersection = (vari, inst, par, vari_next, inst_next, par_next) => {
-
-	let aux = [];
-	let aux2 = [];
-	let aux3 = [];
-	let aux4 = [];
-	let aux5 = [];
-	let aux6 = [];
-	//console.log(inst_next);
-
-	vari.map( value => aux.push(value));
-	inst.map( value => aux2.push(value));
-	par.map( value => aux5.push(value));
-
-	vari_next.map( value => aux3.push(value));
-	inst_next.map( value => aux4.push(value));
-	par_next.map( value => aux6.push(value));
-	
-	console.log(aux.filter((value) => aux2.includes(value) + "\n"));
-	
-	//console.log(aux5.concat(aux,aux2,aux3,aux5));
-	//console.log(aux5);
-
-	for(i in (aux5.filter((value) => aux6.includes(value) + "\n"))) {
-		//if (aux5.filter((value) => aux6.includes(value) + "\n")) {
-			console.log(aux2[i] + " tem conflito com " + aux4[i]);
-		//}
-	} 
-		
-
+var intersection = (o1, o2) => {
+	console.log(_.keys(o1));
+	console.log(_.keys(o2));
+	return _.intersectionBy(_.keys(o1), _.keys(o2), 'parameters');
 	
 }
+
 
