2f97ee8c5e71a600bf60f2d8d0bde50b50b3027c
Allan Ribeiro <allan_paz_ribeiro@hotmail.com>
2019-03-18 17:30:08 -0300

    MEU DEUS QUE SONO

--- assembly_analize.js
+++ assembly_analize.js
@@ -63,38 +63,82 @@ const fs = require('fs');
 const ParseArquivo = () => {
 	fs.readFile('assembly.txt', 'utf8', function(err, data) { 
 	let lines = data.split('\n');
-	let aux = lines.toString().split('\t\tADD\t$T0,$T1,$T2\r');
-	let lines_next = aux.toString().split('\r');
 	var linesObj = [];
-	var linesObjNext = [];
-	lines.map(data => {
+	lines.map(line => {
+		line = line.replace(/\r/g, '');
+		let regexp = /^(?:(?:(\w+)\:\t)|(?:\t\t))(\w{1,4})\t((?:\$\w+)|(?:\w+)|(?:\d))(?:\,((?:\$\w+)|(?:\w+)|(?:\d)))?(?:\,((?:\$\w+)|(?:\w+)|(?:\d)))?$/;
+		let matches = regexp.exec(line);
 		linesObj.push({
-			linesItSelf : data,
-			inst: data.match(/ADDI|ADD|SUB|LW|J|BNE/g),
-			parameters: data.match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g)
+			linesItSelf : line,
+			branch: matches ? matches[1] : '',  
+			inst: matches ? matches[2] : '',
+			parameters: matches ? matches.slice(3) : ''
 		})
+		
 	});
-	lines_next.map(data => {
-		linesObjNext.push({
-			linesItSelf : data,
-			inst: data.match(/ADDI|ADD|SUB|LW|J|BNE/g),
-			parameters: data.match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g)
-		})
-	});	
-		console.log();
-		console.log('-------------intersection---------------------------------------------');
-		console.log(JSON.stringify(linesObj), JSON.stringify(linesObjNext));
-		console.log('----------------------------------------------------------------------');
-		console.log('\n');
+
+	for(linesIndex in linesObj) {
+		intersection(linesObj[linesIndex], linesObj[parseInt(linesIndex) + 1], linesObj[parseInt(linesIndex) + 2]);
+	}
+
+	
+
+	// for(lines in linesObj) 
+	// 	if(linesObj[parseInt(lines)+1])
+			
+	// 			console.log(linesObj[lines].inst + ' tem conflito com ' + linesObj[parseInt(lines)+1].inst);
+	
+
+		// console.log();
+		// console.log('-------------intersection---------------------------------------------');
+		// console.log(JSON.stringify(linesObj));
+		// console.log('----------------------------------------------------------------------');
+		// console.log(JSON.stringify(linesObjNext));
+		// console.log('----------------------------------------------------------------------');
+		//console.log(intersection(linesObj[0], linesObj[1]));
+		// // console.log(intersection(JSON.stringify(linesObj), JSON.stringify(linesObjNext)));
+		// console.log('\n');
 	});
 }
 ParseArquivo();
 
-var intersection = (o1, o2) => {
-	console.log(_.keys(o1));
-	console.log(_.keys(o2));
-	return _.intersectionBy(_.keys(o1), _.keys(o2), 'parameters');
-	
+var intersection = (line1, line2, line3) => {
+	if(line2 && line3){
+		for(let parameter of line2.parameters) {
+			if(line1.inst == 'ADD' || line1.inst == 'SUB' || line1.inst == 'ADDI'){
+
+				if(line1.parameters[0] == parameter)
+					console.log(line1.inst + ' tem conflito com ' + line2.inst);
+
+			} else if(line1.inst == 'SW' || line1.inst == 'LW') {
+				
+				if(line1.parameters[1] == parameter)
+					console.log(line1.inst + ' tem conflito com ' + line2.inst);
+
+				if(line1.parameters[2] == parameter)
+					console.log(line1.inst + ' tem conflito com ' + line2.inst);
+			}	
+					
+		}
+
+		for(let parameter2 of line3.parameters) {
+			if(line1.inst == 'ADD' || line1.inst == 'SUB' || line1.inst == 'ADDI'){
+					
+				if(line1.parameters[0] == parameter2)
+				console.log(line1.inst + ' tem conflito com ' + line3.inst);
+
+			} else if(line1.inst == 'SW' || line1.inst == 'LW'){
+				
+				if(line1.parameters[1] == parameter2)
+				console.log(line1.inst + ' tem conflito com ' + line3.inst);
+
+				if(line1.parameters[2] == parameter2)
+					console.log(line1.inst + ' tem conflito com ' + line3.inst);
+			}
+		}
+	}
+		
+			
 }
 
 
