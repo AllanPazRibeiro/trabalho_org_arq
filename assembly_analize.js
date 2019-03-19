b62e1f90b2be8939e624d23ef8c8393d7bddf560
Allan Ribeiro <allan_paz_ribeiro@hotmail.com>
2019-03-19 11:10:13 -0300

    conseguiiiiiiiiiii ao som de survivor

--- assembly_analize.js
+++ assembly_analize.js
@@ -1,65 +1,4 @@
-
-// const fs = require('fs');
-
-// const leArquivo = () => {
-// 	fs.readFile('assembly.txt', 'utf8', function(err, data) { 
-// 	let lines = data.split('\n');
-// 	var variables_next_arr = [];
-// 	var variables_arr = [];
-// 	var inst_arr = [];
-// 	var inst_next_arr = [];
-
-// 	for (line in lines) {
-// 		//let words = lines[line].split('\t');
-// 		let variables = lines[line].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g);
-// 		let inst = lines[line].match(/ADDI|ADD|SUB|LW|J|BNE/g);
-// 		let variables_next = lines[parseInt(line) + 1] ? lines[parseInt(line)  + 1].match(/((\$[TS])\S?\d,\$[TS]\d)\S((,\$[TS]\d)|(,\w*)\S)?/g) : '';
-// 		let inst2 = lines[parseInt(line)  + 1] ? lines[parseInt(line) + 1].match(/ADDI|ADD|SUB|LW|J|BNE/g) : '';
-// 		inst_next_arr.push(inst2);
-// 		variables_next_arr.push(variables_next);
-// 		inst_arr.push(inst);
-// 		variables_arr.push(variables);
-	
-		
-// 	}		
-// 		console.log('-------------intersection---------------------------------------------');
-// 		intersection(variables_arr, variables_next_arr, inst_arr, inst_next_arr);
-// 		console.log('----------------------------------------------------------------------');
-// 		console.log('\n');
-		
-// 	});
-// }
-// leArquivo();
-
-// //Verifica a intersecção
-// var intersection = (a, b, inst, inst_next) => {
-// 	let aux = [];
-// 	let aux2 = [];
-// 	let aux3 = [];
-// 	let aux4 = [];
-// 	let aux5 = [];
-// 	//console.log(inst_next);
-
-// 	a.map( value => aux.push(value));
-// 	b.map( value => aux2.push(value));
-// 	inst.map( value => aux3.push(value));
-// 	inst_next.map( value => aux4.push(value));
-// 	inst_next.map( value => aux4.push(value));
-
-// 	aux5.concat(aux,aux2,aux3,aux5);
-// 	console.log(aux5.concat(aux,aux2,aux3,aux5));
-
-
-// 	// console.log((aux.filter((value) => aux2.includes(value)+"\n")));
-// 	for (i in (aux.filter((value) => aux2.includes(value) + "\n"))) {
-// 		//if (aux.filter((value) => aux2.includes(value)+"\n"))
-// 		console.log(aux5[i] + " tem conflito com " + aux5[i]);
-// 	}
-	
-// }
-const _ = require('lodash');
 const fs = require('fs');
-
 const ParseArquivo = () => {
 	fs.readFile('assembly.txt', 'utf8', function(err, data) { 
 	let lines = data.split('\n');
@@ -81,29 +20,13 @@ const ParseArquivo = () => {
 		intersection(linesObj[linesIndex], linesObj[parseInt(linesIndex) + 1], linesObj[parseInt(linesIndex) + 2]);
 	}
 
-	
-
-	// for(lines in linesObj) 
-	// 	if(linesObj[parseInt(lines)+1])
-			
-	// 			console.log(linesObj[lines].inst + ' tem conflito com ' + linesObj[parseInt(lines)+1].inst);
-	
-
-		// console.log();
-		// console.log('-------------intersection---------------------------------------------');
-		// console.log(JSON.stringify(linesObj));
-		// console.log('----------------------------------------------------------------------');
-		// console.log(JSON.stringify(linesObjNext));
-		// console.log('----------------------------------------------------------------------');
-		//console.log(intersection(linesObj[0], linesObj[1]));
-		// // console.log(intersection(JSON.stringify(linesObj), JSON.stringify(linesObjNext)));
-		// console.log('\n');
 	});
 }
 ParseArquivo();
 
 var intersection = (line1, line2, line3) => {
-	if(line2 && line3){
+	if(line2) {
+
 		for(let parameter of line2.parameters) {
 			if(line1.inst == 'ADD' || line1.inst == 'SUB' || line1.inst == 'ADDI'){
 
@@ -111,19 +34,20 @@ var intersection = (line1, line2, line3) => {
 					console.log(line1.inst + ' tem conflito com ' + line2.inst);
 
 			} else if(line1.inst == 'SW' || line1.inst == 'LW') {
-				
+
 				if(line1.parameters[1] == parameter)
 					console.log(line1.inst + ' tem conflito com ' + line2.inst);
 
 				if(line1.parameters[2] == parameter)
 					console.log(line1.inst + ' tem conflito com ' + line2.inst);
+				
 			}	
-					
 		}
-
+	}
+	if(line3) {
 		for(let parameter2 of line3.parameters) {
 			if(line1.inst == 'ADD' || line1.inst == 'SUB' || line1.inst == 'ADDI'){
-					
+
 				if(line1.parameters[0] == parameter2)
 				console.log(line1.inst + ' tem conflito com ' + line3.inst);
 
@@ -137,8 +61,7 @@ var intersection = (line1, line2, line3) => {
 			}
 		}
 	}
-		
-			
+
 }
 
 
